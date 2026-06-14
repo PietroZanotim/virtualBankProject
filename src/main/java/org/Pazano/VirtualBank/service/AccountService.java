b28a1a81e4d170cf0fbb50da06244eee7070f74c
Pietro Zanotim <pietroalveszanotim@gmail.com>
2026-06-14 15:20:26 -0300

    Implementing accountService and transactionService layer

--- src/main/java/org/Pazano/VirtualBank/service/AccountService.java
+++ src/main/java/org/Pazano/VirtualBank/service/AccountService.java
@@ -1,7 +1,29 @@
 package org.Pazano.VirtualBank.service;
 
+import org.Pazano.VirtualBank.entities.Account;
+import org.Pazano.VirtualBank.entities.Transaction;
+import org.Pazano.VirtualBank.repository.AccountRepository;
+import org.Pazano.VirtualBank.repository.TransactionRepository;
+import org.Pazano.VirtualBank.service.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AccountService {
+
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public List<Account> findAll() {
+        return accountRepository.findAll();
+    }
+
+    public Account findById(Long id) {
+        Optional<Account> obj = accountRepository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
 }
--- src/main/java/org/Pazano/VirtualBank/service/TransactionService.java
+++ src/main/java/org/Pazano/VirtualBank/service/TransactionService.java
@@ -1,10 +1,27 @@
 package org.Pazano.VirtualBank.service;
 
+import org.Pazano.VirtualBank.entities.Transaction;
+import org.Pazano.VirtualBank.repository.TransactionRepository;
+import org.Pazano.VirtualBank.service.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TransactionService {
 
+    @Autowired
+    private TransactionRepository transactionRepository;
 
+    public List<Transaction> findAll() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction findById(Long id) {
+        Optional<Transaction> obj = transactionRepository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
 
 }
