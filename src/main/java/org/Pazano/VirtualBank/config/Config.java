a99d6303fc3edc1257d0bb3ece7a29487c28403b
Pietro Zanotim <pietroalveszanotim@gmail.com>
2026-08-29 17:27:01 -0300

    Adjusting config.java.

--- src/main/java/org/Pazano/VirtualBank/config/Config.java
+++ src/main/java/org/Pazano/VirtualBank/config/Config.java
@@ -32,14 +32,16 @@ public class Config implements CommandLineRunner {
 
         User u1 = new User(null, "Pietro", "000.000.000-00", "pietro@gmail.com", "19", "123", null);
         User u2 = new User(null, "Ana", "111.111.111-11", "ana@gmail.com", "20", "456", null);
-        userRepository.saveAll(Arrays.asList(u1, u2));
 
         System.out.println(u1.toString());
         System.out.println(u2.toString());
 
         Account a1 = new Account(null, new BigDecimal(1000), u1);
         Account a2 = new Account(null, new BigDecimal(1000), u2);
-        accountRepository.saveAll(Arrays.asList(a1,a2));
+
+        u1.setAccount(a1);
+        u2.setAccount(a2);
+        userRepository.saveAll(Arrays.asList(u1, u2));
 
         Transaction t1 = new Transaction(null, Instant.parse("2026-04-21T17:42:37Z"), a1, u2.getName(), u2.getCpf(), TransactionType.PIX, new BigDecimal(500.00));
         Transaction t2 = new Transaction(null, Instant.parse("2026-04-21T19:52:16Z"), a2, u1.getName(), u1.getCpf(), TransactionType.PIX, new BigDecimal(200.00));
